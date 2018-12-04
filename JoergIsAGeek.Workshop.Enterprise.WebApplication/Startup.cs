9175cba47a29880d17cf4b65fa9f2038c85dbf98
joergkrause <joerg@krause.net>
2018-12-04 22:13:56 +0100

    Move to MicroServices

--- JoergIsAGeek.Workshop.Enterprise.WebApplication/Startup.cs
+++ JoergIsAGeek.Workshop.Enterprise.WebApplication/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -20,16 +21,20 @@ using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JoergIsAGeek.Workshop.Enterprise.WebApplication {
-  public class Startup {
+namespace JoergIsAGeek.Workshop.Enterprise.WebApplication
+{
+  public class Startup
+  {
 
 
-    public Startup(IHostingEnvironment env) {
+    public Startup(IHostingEnvironment env)
+    {
       var builder = new ConfigurationBuilder()
           .SetBasePath(env.ContentRootPath)
           .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
@@ -41,7 +46,8 @@ namespace JoergIsAGeek.Workshop.Enterprise.WebApplication {
     public IConfigurationRoot Configuration { get; }
 
     // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services) {
+    public void ConfigureServices(IServiceCollection services)
+    {
       // Add framework services.
       services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
       services.AddMvc();
@@ -70,7 +76,8 @@ namespace JoergIsAGeek.Workshop.Enterprise.WebApplication {
       services.AddScoped<SignInManager<UserViewModel>, SignInManager<UserViewModel>>();
       services.AddScoped<IJwtFactory, JwtFactory>();
       // user account settings, consider moving to config file
-      services.Configure<IdentityOptions>(options => {
+      services.Configure<IdentityOptions>(options =>
+      {
         // Password settings
         options.Password.RequireDigit = true;
         options.Password.RequiredLength = 8;
@@ -97,14 +104,16 @@ namespace JoergIsAGeek.Workshop.Enterprise.WebApplication {
       var SecretKey = Configuration.GetSection("Keys").GetValue<string>("TokenSecret");
       var _signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SecretKey));
 
-    // TODO: Needed? Configure JwtIssuerOptions
-    services.Configure<JwtIssuerOptions>(options => {
+      // TODO: Needed? Configure JwtIssuerOptions
+      services.Configure<JwtIssuerOptions>(options =>
+      {
         options.Issuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)];
         options.Audience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)];
         options.SigningCredentials = new SigningCredentials(_signingKey, SecurityAlgorithms.HmacSha256);
       });
 
-      var tokenValidationParameters = new TokenValidationParameters {
+      var tokenValidationParameters = new TokenValidationParameters
+      {
         // The signing key must match!
         ValidateIssuerSigningKey = true,
         ValidateAudience = false,
@@ -117,25 +126,30 @@ namespace JoergIsAGeek.Workshop.Enterprise.WebApplication {
 
       // Per default failed requests redirect to Account/Logon, 
       // but here we have SPA with API and need to inform SPA app to handle this.
-      services.AddAuthentication(options => {
+      services.AddAuthentication(options =>
+      {
         options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
         options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
         options.DefaultSignInScheme = JwtBearerDefaults.AuthenticationScheme;
         options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
         options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-      }).AddJwtBearer(options => {
+      }).AddJwtBearer(options =>
+      {
         options.IncludeErrorDetails = true;
         options.TokenValidationParameters = tokenValidationParameters;
 #pragma warning disable CS1998 // #warning directive for await/async violation, it's just while debug code is in here
-        options.Events = new JwtBearerEvents {
+        options.Events = new JwtBearerEvents
+        {
           OnMessageReceived = async (context) =>
           {
             Debug.WriteLine("====>  JWT Message received");
           },
-          OnTokenValidated = async (context) => {
+          OnTokenValidated = async (context) =>
+          {
             Debug.WriteLine("====>  JWT token validated");
           },
-          OnAuthenticationFailed = c => {
+          OnAuthenticationFailed = c =>
+          {
             Debug.WriteLine($"====>  JWT auth failed: {c.Exception}");
             c.NoResult();
             c.Response.StatusCode = 403;
@@ -143,24 +157,44 @@ namespace JoergIsAGeek.Workshop.Enterprise.WebApplication {
             c.Response.WriteAsync(c.Exception.ToString()).Wait();
             return Task.CompletedTask;
           },
-          OnChallenge = c => {
+          OnChallenge = c =>
+          {
             c.HandleResponse();
             return Task.CompletedTask;
           }
         };
 #pragma warning restore CS1998 // #warning directive
       });
-      services.AddAuthorization(options => {
+      services.AddAuthorization(options =>
+      {
         options.DefaultPolicy = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme)
         .RequireAuthenticatedUser()
         .Build();
         // API users just need to have this particular claim to use the API        
-        options.AddPolicy("ApiUser", policy => {
+        options.AddPolicy("ApiUser", policy =>
+        {
           // this is in Roles/UserRoles and connecting it to the policy simplifies the [Authoize] attribute
           policy.RequireRole("User");
           // this is in the UserClaims table connected to particular users. weirdguest has no access, all others have access
           policy.RequireClaim("api_access");
         });
+      });
+      // TODO: http://bitoftech.net/2014/12/15/secure-asp-net-web-api-using-api-key-authentication-hmac-authentication/
+      // common API options
+      services.Configure<ApiBehaviorOptions>(options =>
+      {
+        options.InvalidModelStateResponseFactory = actionContext =>
+        {
+          var errors = actionContext.ModelState
+              .Where(e => e.Value.Errors.Count > 0)
+              .Select(e => new
+              {
+                Name = e.Key,
+                Message = e.Value.Errors.First().ErrorMessage
+              }).ToArray();
+
+          return new BadRequestObjectResult(errors);
+        };
       });
 
       // support for object mappings
@@ -170,36 +204,45 @@ namespace JoergIsAGeek.Workshop.Enterprise.WebApplication {
     /// <summary>
     /// Unauthenticated ajax or API request returns 403 rather than Redirect to forbidden page
     /// </summary>
-    private static Task DontRedirectAjaxOrApiRequestToForbidden(AuthenticationFailedContext ctx) {
+    private static Task DontRedirectAjaxOrApiRequestToForbidden(AuthenticationFailedContext ctx)
+    {
       bool isAjaxRequest = ctx.HttpContext.Request.Headers["x-requested-with"] == "XMLHttpRequest";
-      if (isAjaxRequest || (ctx.Request.Path.StartsWithSegments("/api"))) {
+      if (isAjaxRequest || (ctx.Request.Path.StartsWithSegments("/api")))
+      {
         ctx.Response.StatusCode = 403; // this is where the Angular interceptor comes into the game (401 will work, too)
       }
-      else {
+      else
+      {
         ctx.Response.StatusCode = 400; // API only
       }
       return Task.CompletedTask;
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory) {
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
 
-      if (env.IsDevelopment()) {
+      if (env.IsDevelopment())
+      {
         app.UseDeveloperExceptionPage();
       }
-      else {
+      else
+      {
         // TODO: Figure out how to handle global errors with SPA front end??
         app.UseExceptionHandler("/Home/Error");
       }
 
       app.UseExceptionHandler(
-      builder => {
-        builder.Run(async context => {
+      builder =>
+      {
+        builder.Run(async context =>
+        {
           context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
           context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
 
           var error = context.Features.Get<IExceptionHandlerFeature>();
-          if (error != null) {
+          if (error != null)
+          {
             context.Response.AddApplicationError(error.Error.Message);
             await context.Response.WriteAsync(error.Error.Message).ConfigureAwait(false);
           }
