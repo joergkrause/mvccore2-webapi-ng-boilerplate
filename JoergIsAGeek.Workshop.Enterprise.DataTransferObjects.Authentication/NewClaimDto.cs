c2731b25bb00fe5d30b88dccb06138ff2671a71d
joergkrause <joerg@krause.net>
2018-12-05 22:19:20 +0100

    Moved Transferobjects to singular projects inside micro service

--- JoergIsAGeek.Workshop.Enterprise.DataTransferObjects.Authentication/NewClaimDto.cs
+++ JoergIsAGeek.Workshop.Enterprise.DataTransferObjects.Authentication/NewClaimDto.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JoergIsAGeek.Workshop.Enterprise.DataTransferObjects.Authentication {
+﻿namespace JoergIsAGeek.Workshop.Enterprise.DataTransferObjects.Authentication
+{
 
   /// <summary>
   /// A special transfer object that overcomes parameter limitations in Open API.
